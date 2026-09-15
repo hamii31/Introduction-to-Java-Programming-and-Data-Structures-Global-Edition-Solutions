a8a73cf09b28c15107f712a54dfc2db69cf0a3be
Hami Ibriyamov <hamop3108@gmail.com>
2026-09-15 11:50:25 +0200

    Add loop to display 'I love Java' multiple times

--- Chapter I/Display.java
+++ Chapter I/Display.java
@@ -4,5 +4,9 @@ public class Display
         System.out.println("Welcome to Java");
         System.out.println("Learning Java Now");
         System.out.println("Programming is fun");
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("I love Java");
+        }
     }
 }
