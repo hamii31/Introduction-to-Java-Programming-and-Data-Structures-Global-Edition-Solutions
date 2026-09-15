e92d02f517ac5390541d175808b52a3ca98f5f00
Hami Ibriyamov <hamop3108@gmail.com>
2026-09-15 11:56:15 +0200

    Add multiple print statements and calculations

--- Chapter I/Display.java
+++ Chapter I/Display.java
@@ -1,17 +1,30 @@
 public class Display
 {
     public static void main(String[] args){
+        //1.1
         System.out.println("Welcome to Java");
         System.out.println("Learning Java Now");
         System.out.println("Programming is fun");
 
+        //1.2
         for (int i = 0; i < 5; i++){
             System.out.println("I love Java");
         }
 
+        //1.3
         System.out.println("   J ");
         System.out.println("J aaa  v  vaaa");
         System.out.println("J  J aa vv a a");
         System.out.println("J aaaa   v  aaaa");
+
+        //1.4
+        System.out.println("aa^2a^3a^4");
+        System.out.println("1 1  1  1");
+        System.out.println("2 4  8  16");
+        System.out.println("3 9 27  81");
+        System.out.println("4 16 64 256");
+
+        //1.5
+        System.out.println((7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5));
     }
 }
