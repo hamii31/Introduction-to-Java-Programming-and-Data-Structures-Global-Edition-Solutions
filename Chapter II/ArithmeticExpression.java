b1a3133ecef80ef1e35f90d33ee3dbc2d6e45375
Hami Ibriyamov <hamop3108@gmail.com>
2026-09-19 11:05:25 +0200

    Create ArithmeticExpression.java

---
+++ Chapter II/ArithmeticExpression.java
@@ -0,0 +1,8 @@
+public class ArithmeticExpression {
+    public static void main(String[] args) {
+        int r = 1, a = 1, b = 1, c = 1, d = 1, t = 1;
+
+        System.out.println(4 / (3 * (r + 34)) - 9 * (a + Math.abs(b * c)) + (3 + d * (2 + a)) / (a + b * d));
+        System.out.println(5.5 * Math.pow((r + 2.5), 2.5 + t));
+    }
+}
